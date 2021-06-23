558b21d81501a3542a44d5d45a03d7df9216f295
calicowhoman <mikeyshrier@gmail.com>
2021-06-23 13:56:27 -0700

    4 did

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ *  * main - echo the Programming... and return 0
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-char Lemons[] = "Programming is like building a multilingual puzzle";
+char Lemons[] = "\"Programming is like building a multilingual puzzle";
 puts(Lemons);
 
 return (0);
