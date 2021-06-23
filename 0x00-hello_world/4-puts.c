2842482e1cab3596d991279c432889891d4200f8
calicowhoman <mikeyshrier@gmail.com>
2021-06-23 13:47:29 -0700

    4 agains

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-char Lemons[] = "Programming is like building a multilingual puzzle";
+char Lemons[] = "Programming is like building a multilingual puzzle;
 puts(Lemons);
 
 return (0);
