40653eca11a8efdacf7e1396782b689001e03afb
calicowhoman <mikeyshrier@gmail.com>
2021-06-23 14:24:47 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-#commment
+#include <stdio.h>
+/**
+ *  * main - echo the Programming... and return 0
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	char Lemons[] = "with proper grammar, but the outcome is a piece of art,\n";
+	printf(Lemons);
+
+	return (0);
+}
