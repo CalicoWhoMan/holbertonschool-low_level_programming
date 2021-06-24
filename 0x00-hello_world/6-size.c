b5ec545fa7b5f4a9f061eec3ae59dedb366905e4
calicowhoman <mikeyshrier@gmail.com>
2021-06-23 17:18:05 -0700

    6 first

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,22 @@
-#!/bin/bash
-#commment
+#include <stdio.h>
+/**
+ * main - echo the Programming... and return 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longint;
+	long long int longlongint;
+	float floatType;
+
+	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of longint: %zu bytes\n", sizeof(longint));
+	printf("Size of longlongint: %zu bytes\n", sizeof(longlongint));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+
+	return (0);
+}
