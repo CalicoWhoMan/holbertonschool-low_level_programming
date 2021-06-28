58c0e7b922c8f3eb1d34650405e9c1a8473d5c76
calicowhoman <mikeyshrier@gmail.com>
2021-06-28 11:09:33 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints single digit nums of base 10 from 0 with new line
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	int lemon;
+
+	for (lemon = 0 ; lemon < 10 ; lemon++)
+	{
+		printf("%d", lemon);
+	}
+	return (0);
+}
