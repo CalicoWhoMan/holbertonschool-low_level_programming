8819d38d9f7f3792d2f2493d098cd995fae217a7
calicowhoman <mikeyshrier@gmail.com>
2021-06-28 11:24:18 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - single digits of base 10 starting from 0
+ *
+ * return: 0
+ */
+
+int main(void)
+{
+	int g;
+	{
+	for (g = 0; g < 10; g++)
+	putchar(g + '0');
+	}
+	putchar ('\n');
+	return (0);
+}
