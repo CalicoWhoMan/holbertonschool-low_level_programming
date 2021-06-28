51a1cae90a2fd50566f1dfafb691d248450186a3
calicowhoman <mikeyshrier@gmail.com>
2021-06-28 12:24:22 -0700

    9 e

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,12 @@ int main(void)
 	int a;
 
 	for (a = '0'; a <= '9'; a++)
-	{
-		if (a != '9')
-			continue;
-		{
+		putchar ((a % 10) + '0');
+	if (a != '9')
+{
 	putchar (',');
 	putchar (' ');
-		}
+
 	}
 
 	putchar ('\n');
