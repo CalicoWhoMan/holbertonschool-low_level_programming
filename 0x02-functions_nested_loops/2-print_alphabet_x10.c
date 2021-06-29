05a7a7b1fa6aebb2295ef987660be034a9bf7f61
calicowhoman <mikeyshrier@gmail.com>
2021-06-29 15:22:12 -0700

    2 aga

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,50 +2,27 @@
 /**
  * print_alphabet_x10 - check the code for Holberton School students.
  *
- * Return: 
+ * Return: void
  */
 
 void print_alphabet_x10(void)
+
 {
 	int lemon = 0;
 	char lime;
-	
-	while (lemon++ <=10)
+
+	while (lemon++ <= 10)
+
 	
 	for (lime = 'a'; lime <= 'z'; lime++)
-	
+
 	{
 	_putchar (lime);
+
 	}
+	{
 	_putchar ('\n');
-}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	}
 
 }
