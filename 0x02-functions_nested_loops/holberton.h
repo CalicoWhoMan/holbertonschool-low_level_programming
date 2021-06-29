0ff8b40d6bf4fb83c3870531573a84d0f08f4342
calicowhoman <mikeyshrier@gmail.com>
2021-06-29 12:17:14 -0700

    h1

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,1 +1,12 @@
-int _putchar(char c);
+int _putchar(char c);/*0*/
+void print_alphabet(void);/*1*/
+void print_alphabet_x10(void);/*2*/
+int _islower(int c);/*3*/
+int _isalpha(int c);/*4*/
+int print_sign(int n);/*5*/
+int _abs(int);/*6*/
+int print_last_digit(int);/*7*/
+void jack_bauer(void);/*8*/
+void times_table(void);/*9*/
+int add(int, int);/*10*/
+void print_to_98(int n);/*11*/
