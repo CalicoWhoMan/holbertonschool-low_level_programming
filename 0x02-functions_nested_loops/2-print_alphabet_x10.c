1d6d26c6cf3d707377c9e5498200dde29dbe592d
calicowhoman <mikeyshrier@gmail.com>
2021-06-29 15:09:04 -0700

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - check the code for Holberton School students.
+ *
+ * Return: 
+ */
+
+void print_alphabet_x10(void)
+{
+	int lemon = 0;
+	char lime;
+	
+	while (lemon++ <=10)
+	
+	for (lime = 'a'; lime <= 'z'; lime++)
+	
+	{
+	_putchar (lime);
+	}
+	_putchar ('\n');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
