59ea1ba17aaa29d26e64fd789650cb91d13b7e72
calicowhoman <mikeyshrier@gmail.com>
2021-06-29 11:57:02 -0700

    aksda

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+
+{
+	int lemons;
+
+	for (lemons = 'a'; lemons <= 'z'; ++lemons)
+	{
+		_putchar (lemons);
+	}
+	{
+	_putchar('\n');
+	}
+return (0);
+}
