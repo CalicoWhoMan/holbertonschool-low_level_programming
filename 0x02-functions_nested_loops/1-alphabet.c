789ec32ca83abe8245c24e46ae1e0d52e3aba864
calicowhoman <mikeyshrier@gmail.com>
2021-06-29 12:13:57 -0700

    1 try

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - check the code for Holberton School students.
  *
- * Return: (0)
+ * Return: void
  */
 
 void print_alphabet(void)
@@ -17,5 +17,4 @@ void print_alphabet(void)
 	{
 	_putchar('\n');
 	}
-return (0);
 }
