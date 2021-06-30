8f7cfc2bd18bd13f985b4866f3843a643e239e40
calicowhoman <mikeyshrier@gmail.com>
2021-06-29 17:00:14 -0700

    4 ag

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _islower - check the code for Holberton School students.
+ *
+ * @c: - checking
+ *
+ * Return: 1 otherwise 0
+ */
+
+int _isalpha(int c)
+
+{
+
+	if (c >= 97 && c <= 122)
+	if (c >= 65 && c <= 90)
+
+	return (1);
+	else
+	return (0);
+}
