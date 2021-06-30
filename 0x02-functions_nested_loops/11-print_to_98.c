70525016bab88fd54d17217ec3d558af37032c95
calicowhoman <mikeyshrier@gmail.com>
2021-06-29 21:45:33 -0700

    last chance

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * print_to_98 - prints natural nums to 98 \n
+ * @n: number
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
