eb623cca0935d8b3cc7795aa49248c47d19951c6
calicowhoman <mikeyshrier@gmail.com>
2021-06-29 20:28:07 -0700

    6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * abs - funcation that computes the absolute value of integer
+ * @a: this
+ * Return: 0
+ */
+
+int _abs(int a)
+{
+if (a < 0)
+
+	return (-a);
+	else
+	return (a);
+}
+
