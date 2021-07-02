8c4b3c54aefe2e0956b1a615f7c9eca89ff1c0ba
calicowhoman <mikeyshrier@gmail.com>
2021-07-01 19:45:32 -0700

    3 warnming

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints nums from 0 to 9 with a new line
+ *
+ * Return: 0-9
+ */
+
+void print_numbers(void)
+
+{
+int ch;
+
+for (ch = 0; ch <= 9; ch++)
+
+{
+	_putchar(ch + '0');
+}
+
+	_putchar('\n');
+
+}
