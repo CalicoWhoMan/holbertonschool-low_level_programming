3bb715e375aa565b05f82403fbf983f03cbb55cc
calicowhoman <mikeyshrier@gmail.com>
2021-07-06 14:43:24 -0700

    git e

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,10 +14,9 @@ void puts2(char *str)
 	while (str[len] != 0)
 
 	{
-		if ((len % 2) == 0)
-		{
-			putchar(str[len]);
-		}
+		if (len % 2 == 0)
+		_putchar(str[len]);
+	len++;
 	}
 	_putchar('\n');
 }
