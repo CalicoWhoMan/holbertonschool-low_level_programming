6756f4efa2c4658309319675ecc3b3a8734b4b1b
calicowhoman <mikeyshrier@gmail.com>
2021-07-05 22:29:36 -0700

    6 strlen

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character of a string and\n
+ * @str: string
+ */
+
+void puts2(char *str)
+
+{
+	int len = 0, rev = 0;
+
+	len = _strlen(str);
+
+	for (int rev = 0; rev < len; rev += 2)
+{
+	putchar(str[rev]);
+}
+	_putchar('\n');
+}
