de3ae33582cd69a20946ccf64273a855663a0bad
calicowhoman <mikeyshrier@gmail.com>
2021-07-06 14:07:48 -0700

    6 a

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,12 @@
 void puts2(char *str)
 
 {
-	int i = 0, len = 0;
+	int i, len = 0;
 
-	while (str[i])
+	while (str[len] != '\0')
+	{
 		len++;
-
+	}
 	for (i = 0; i < len; i += 2)
 	{
 		putchar(str[i]);
