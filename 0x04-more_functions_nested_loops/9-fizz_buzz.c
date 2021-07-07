c426fe19989e3287536851c9bbf5d087f4cbeb86
calicowhoman <mikeyshrier@gmail.com>
2021-07-07 08:26:00 -0700

    FizzBuzzy

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,44 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
+ * main - descrip for the main function
  *
- *
- *
- *
- *
- *
- *
+ * Return: 0
  */
 
+int main(void)
 
+{
+int num;
+for (num = 1; num <= 100; num++)
+{
+
+	if (num % 3 && num % 5 == 0)
+	{
+		printf("FizzBuzz ");
+	}
+
+	else if (num % 3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else if (num % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	else
+	{
+		printf("%i", num);
+	}
+	if (num < 100)
+	{
+		printf(" ");
+	}
+	else
+	{
+		printf("\n");
+
+	}
+}
+	return (0);
+}
