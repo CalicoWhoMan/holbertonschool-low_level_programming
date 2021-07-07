743eb2e75c54662b308aab6c1446407b7d2faa6b
calicowhoman <mikeyshrier@gmail.com>
2021-07-06 20:32:51 -0700

    print half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,13 +1,27 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
- *
- *
- *
- *
+ * puts_half - print half of a string
+ * @str: string
  */
 
+void puts_half(char *str)
+{
 
+int beg = 0, mid = 0, end = 0;
+
+while (str[beg] != 0)
+beg++;
+if (beg % 2 == 0)
+mid = beg / 2;
+
+else
+mid = (beg + 1) / 2;
+for (end = mid; end < beg; end++)
+{
+_putchar(str[end]);
+}
+{
+_putchar('\n');
+}
+}
