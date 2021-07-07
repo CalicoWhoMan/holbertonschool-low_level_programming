27ee983a5b6b079c1841be1ab721b8baed08d601
calicowhoman <mikeyshrier@gmail.com>
2021-07-07 08:28:27 -0700

    abc

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,12 +15,12 @@ for (num = 1; num <= 100; num++)
 
 	if (num % 3 && num % 5 == 0)
 	{
-		printf("FizzBuzz ");
+		printf("FizzBuzz");
 	}
 
 	else if (num % 3 == 0)
 	{
-		printf("Fizz ");
+		printf("Fizz");
 	}
 	else if (num % 5 == 0)
 	{
