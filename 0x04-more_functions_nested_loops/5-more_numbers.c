27ef72946a9058994129237c43b61f117d0da0f6
calicowhoman <mikeyshrier@gmail.com>
2021-07-07 19:17:18 -0700

    '0' added instead

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,24 @@
 #include "holberton.h"
 /**
- *
- *
- *
- *
- *
- *
- *
+ * more_numbers - print 0-14 10 times with new line
  */
 
-void more_numbers(void);
+void more_numbers(void)
+{
+	int num, ag;
+
+	for (num = 0; num <= 9; num++)
+	{
+	for (ag = 0; ag <= 14; ag++)
+	{
+	if (ag >= 10)
+		{
+		_putchar ((ag / 10) + '0');
+		}
+		_putchar ((ag % 10) + '0');
+	}
+		_putchar('\n');
+
+	}
+
+}
