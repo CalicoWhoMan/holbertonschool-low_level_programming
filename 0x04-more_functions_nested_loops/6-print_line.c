62d2e0b2d95553e5c4f539f7155db52f51d2a2b6
calicowhoman <mikeyshrier@gmail.com>
2021-07-07 20:57:38 -0700

    underscore little spaces

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,19 @@
 #include "holberton.h"
 /**
+ * print_line - prints a underscore
  *
- *
- *
- *
- *
- *
- *
+ * @n: number of underscores
  */
 
-void print_line(int n);
+void print_line(int n)
+{
+int u;
+if (n >= 0)
+{
+for (u = 0; u < n; u++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
