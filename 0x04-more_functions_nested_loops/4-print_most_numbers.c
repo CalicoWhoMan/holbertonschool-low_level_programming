982f24243135dbf2342be639566f3ee8bd2cf1bc
calicowhoman <mikeyshrier@gmail.com>
2021-07-07 18:56:41 -0700

    4 comit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
 /**
- *
- *
- *
- *
- *
- *
+ * print_most_numbers - prints numbers 0 to 9 execpt
+ * for 2 and 4 and new line
  */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
+{
+int num;
+	for (num = 48; num <= 57; num++)
+	{
+	if (num != 50 && num != 52)
+	{
+		_putchar(num);
+	}
+	}
+		_putchar('\n');
+
+}
