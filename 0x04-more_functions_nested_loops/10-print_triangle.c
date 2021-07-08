38dcd096a05ffd26259ca262cae130f4f651b43c
calicowhoman <mikeyshrier@gmail.com>
2021-07-07 21:15:57 -0700

    7

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,16 +1,28 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
+ * print_triangle - print a half triangle
  *
+ * @size: the size of half triangle
  *
- *
- *
- *
- *
- *
+ * Return: void
  */
 
-
-
-void print_triangle(int size);
-
+void print_triangle(int size)
+{
+int i, j;
+if (size <= 0)
+_putchar('\n');
+for (i = 1; i <= size; ++i)
+{
+for (j = 1; j <= size; ++j)
+{
+if (j <= (size - i))
+_putchar(' ');
+else
+_putchar('#');
+}
+_putchar('\n');
+}
+}
 
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,13 +1,28 @@
 #include "holberton.h"
 /**
+ * print_diagonal - prints diagonal in term
  *
- *
- *
- *
- *
- *
- *
+ * @n: times to be printed either space or slash
  */
 
-void print_diagonal(int n);
+void print_diagonal(int n)
+{
 
+int key, my;
+
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
+
+for (my = 0; my < n; my++)
+{
+for (key = 0; key < my; key++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
