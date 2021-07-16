777ba71aad85f52b38311cb75c7906770ac78a3d
calicowhoman <mikeyshrier@gmail.com>
2021-07-15 22:02:24 -0700

    betty fixed

--- 0x0B-argc_argv/0-whatsmyname.c
+++ 0x0B-argc_argv/0-whatsmyname.c
@@ -2,20 +2,19 @@
 #include <stdio.h>
 
 /**
- * main: function 
+ * main - function
  * @argc: integer
  * @argv: string value
- * 
  * Return: 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 int i;
 
-for(i = 0; i < argc; i++)
+for (i = 0; i < argc; i++)
 {
 printf("%s\n", argv[0]);
 }
-return 0;
+return (0);
 }
