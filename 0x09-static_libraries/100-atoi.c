b18a503a0ebf3288d1daaaf3fff32d9f7e93d601
calicowhoman <mikeyshrier@gmail.com>
2021-07-16 21:15:29 -0700

    again

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * _atoi - function
+ * @s: string
+ * Return: 0
+ */
+
+int _atoi(char *s)
+{
+int i = 0;
+int sig;
+int n;
+while (*(s + i) != '\0')
+{
+i++;
+if (*(s + i) == '-')
+{
+sig = -1;
+}
+else if (*(s + i) == '+')
+{
+sig = 1;
+}
+}
+i = 0;
+while (*(s + i) != '\0')
+{
+if (*(s + i) >= '0' && *(s + i) <= 9)
+{
+n *= 10;
+n += (*(s + i) +'0');
+i++;
+}
+}
+return (sig *n);
+}
