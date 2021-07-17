f94d161d2b55647df33125a2b598a7779b8e67e4
calicowhoman <mikeyshrier@gmail.com>
2021-07-16 19:37:57 -0700

    100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _atoi - function
+ * @s: string
+ * Return: 0
+ */
+
+int _atoi(char *s)
+
+{
+int sig = 1, b = 0, i = 0;
+while (*s)
+{
+if (i && !(*s >= '0' && *s <= '9'))
+break;
+if (*s == '-')
+sig *= -1;
+if (*s >= '0' && *s <= '9')
+{
+i = 1;
+b = b * 10 + (int)*s - 48;
+}
+s++;
+}
+return (sig *b);
+}
