ab35a960ef99c24c8f78982737e03a9bdcb2b023
calicowhoman <mikeyshrier@gmail.com>
2021-07-20 21:14:58 -0700

    fix

--- 0x0C-malloc_free/1-strdup.c
+++ 0x0C-malloc_free/1-strdup.c
@@ -3,36 +3,30 @@
 #include <stdlib.h>
 
 /**
- * strdup - funtion to that returns a pointer to a newly allocated space in
+ * _strdup - funtion to that returns a pointer to a newly allocated space in
  * memory, which contains a copy of the string given as a parameter.
- *
  * @str: string
  * Return: NULL or string/pointer
- *
  */
 
 char *_strdup(char *str)
 {
-	char *n;
-	int a, i;
+char *n;
+int a, i;
 
-	if (str != NULL)
-	{
-		for (i = 0; str[i];  i++)
-			continue;
-	n = malloc(i + 1);
-	if (n != NULL)
-	{
-	for (a = 0; a < i; a++)
-
-		n[a] = str[a];
-	}
-	}
-	return (n);
-
-
-
-
-
-
+for (i = 0; str[i];  i++)
+{
+continue;
+}
+n = malloc((i * sizeof(char)) +  1);
+if (n == NULL || str == NULL)
+{
+return (NULL);
+}
+for (a = 0; a < i; a++)
+{
+n[a] = str[a];
+}
+n[i] = '\0';
+return (n);
 }
