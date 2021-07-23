daaad2ec7147271cc2646b908134a63ffd4b6c4d
calicowhoman <mikeyshrier@gmail.com>
2021-07-23 14:02:26 -0700

    hope fixed segmentation fault (core dumped)

--- 0x0E-more_malloc_free/3-array_range.c
+++ 0x0E-more_malloc_free/3-array_range.c
@@ -1,23 +1,31 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
- *
- *
+ * array_range - func that creates an array of ints
+ * @min: minimum amt of ints
+ * @max: maximum amt of ints
+ * Return: pointer
  */
 
 int *array_range(int min, int max)
 
 {
+int *point, i;
 
+if (min > max)
+{
+return (NULL);
+}
+point = malloc(((max - min) + 1) * sizeof(int));
 
-
-
-
-
-
-
-
+if (point == NULL)
+{
+return (NULL);
+}
+for (i = 0; min <= max; i++)
+{
+point[i] = min;
+min++;
+}
+return (point);
 }
