ccb79c72e035674208638d08016c4d6b47eb455b
calicowhoman <mikeyshrier@gmail.com>
2021-07-23 13:12:34 -0700

    nice

---
+++ 0x0E-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _calloc - func that allocates mem of an array
+ * @nmemb: array
+ * @size: bytes
+ * Return: s1 followed by n bytes of s2 and NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i;
+char *x;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+x = malloc(nmemb * size);
+if (x == NULL)
+return (NULL);
+for (i = 0; i < nmemb; i++)
+x[i] = 0;
+return (x);
+}
