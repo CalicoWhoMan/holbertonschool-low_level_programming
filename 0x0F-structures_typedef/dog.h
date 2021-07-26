35199c9f5e8dd20c832d94b4470287aa301bce19
calicowhoman <mikeyshrier@gmail.com>
2021-07-26 15:50:25 -0700

    owner desc

--- 0x0F-structures_typedef/dog.h
+++ 0x0F-structures_typedef/dog.h
@@ -5,7 +5,7 @@
  * struct dog - name, age, and owner of dog
  * @name: name var
  * @age: age var
- * owner: owner var
+ * @owner: owner var
  */
 
 struct dog
@@ -14,8 +14,8 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
-dog_t;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
