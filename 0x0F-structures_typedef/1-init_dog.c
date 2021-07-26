fb4ae3c1c0987e6878192ecfecd1f94d3d17bbd4
calicowhoman <mikeyshrier@gmail.com>
2021-07-26 16:23:52 -0700

    1

---
+++ 0x0F-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stddef.h>
+
+/**
+ * init_dog - name, age, and owner of dog
+ * @d: structure
+ * @name: name var
+ * @age: age var
+ * @owner: owner var
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+
+{
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
