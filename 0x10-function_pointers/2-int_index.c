9151a2ee769464a2b4853a692d61ee5ff6198955
calicowhoman <mikeyshrier@gmail.com>
2021-07-27 12:36:17 -0700

    2

---
+++ 0x10-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - funct that searches for an int
+ * @array: array
+ * @size: # of elements in the array
+ * @cmp: pointer to func to compare values
+ * Return: -1 or index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+
+{
+
+	int i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; ++i)
+		return (-1);
+}
