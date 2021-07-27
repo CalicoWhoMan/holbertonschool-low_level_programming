d5decf8b84928bc338ae244d6bc787e19a8c0ccd
calicowhoman <mikeyshrier@gmail.com>
2021-07-27 12:39:43 -0700

    try gain

--- 0x10-function_pointers/2-int_index.c
+++ 0x10-function_pointers/2-int_index.c
@@ -11,11 +11,14 @@
 int int_index(int *array, int size, int (*cmp)(int))
 
 {
+int i;
 
-	int i;
-
-	if (size <= 0 || array == NULL || cmp == NULL)
-		return (-1);
-	for (i = 0; i < size; ++i)
-		return (-1);
+if (size <= 0 || array == NULL || cmp == NULL)
+return (-1);
+for (i = 0; i < size; ++i)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1);
 }
