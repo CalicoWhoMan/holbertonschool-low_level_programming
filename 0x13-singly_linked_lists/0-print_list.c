e8fc941506c9f25d8fc4ae265488038e7a6db476
calicowhoman <mikeyshrier@gmail.com>
2021-08-06 19:58:59 -0700

    added 0 header and putchar

---
+++ 0x13-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of list_t
+ * @h: pointer
+ * Return: the nodes
+ */
+
+size_t print_list(const list_t *h)
+
+{
+	unsigned int node = 0;
+	const list_t *first_l = h;
+
+	while (first_l)
+	{
+		if (first_l->str == NULL)
+			printf("[0] (nil\n");
+		else
+			printf("[%d] %s\n", first_l->len, first_l->str);
+	node++;
+	first_l = first_l->next;
+	}
+	return (node);
+}
