3f20dcc791f8bd2fb71c47e129ec3daa49f5ac71
calicowhoman <mikeyshrier@gmail.com>
2021-08-06 21:42:23 -0700

    add node + betty

---
+++ 0x13-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of list_t
+ * @head: pointer
+ * @str: pointer
+ * Return: the address of the new element or NULL
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *point;
+
+	point = (list_t *)malloc(sizeof(list_t));
+
+	if (str == NULL)
+		return (NULL);
+	point = (list_t *)malloc(sizeof(list_t));
+
+	point->str = strdup(str);
+	point->len = strlen(str);
+	point->next = *head;
+
+	*head = point;
+	return (point);
+}
