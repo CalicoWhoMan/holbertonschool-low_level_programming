ae6b8a9d98a9d7ab78e1815da7b625dcecc1a980
calicowhoman <mikeyshrier@gmail.com>
2021-08-11 11:16:14 -0700

    func to add a new node at at end of list

---
+++ 0x14-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: double pointer
+ * @n: const int
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+
+{
+	listint_t *insomnia;
+	listint_t *awake = *head;
+
+	insomnia = malloc(sizeof(listint_t));
+	if (insomnia == NULL)
+		return (NULL);
+	insomnia->n = n;
+	insomnia->next = NULL;
+
+	if (*head == NULL)
+		*head = insomnia;
+	else
+	{
+		while (awake->next != NULL)awake = awake->next;
+		awake->next = insomnia;
+	}
+	return (insomnia);
+}
