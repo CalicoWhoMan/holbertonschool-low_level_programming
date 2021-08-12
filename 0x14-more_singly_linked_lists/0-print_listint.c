215d94b2ceb3f65c4f9eff822271070e37452468
calicowhoman <mikeyshrier@gmail.com>
2021-08-11 17:07:32 -0700

    reverse reverse

--- 0x14-more_singly_linked_lists/0-print_listint.c
+++ 0x14-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stddef.h>
 #include <stdio.h>
+
 /**
  * print_listint - prints all the elements of a listin_t list
  * @h: pointer
@@ -12,14 +13,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t insomnia = 0;
 
-	if (!h)
-		return (0);
-
 	while (h != NULL)
 	{
+		insomnia++;
 		printf("%i\n", h->n);
 		h = h->next;
-		insomnia++;
 	}
 	return (insomnia);
 }
