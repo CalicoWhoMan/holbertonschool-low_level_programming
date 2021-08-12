d51aa2dd8d12d15466a7a82f0d7c19d1a67bf6b7
calicowhoman <mikeyshrier@gmail.com>
2021-08-11 19:13:38 -0700

    fixed betty

--- 0x14-more_singly_linked_lists/7-get_nodeint.c
+++ 0x14-more_singly_linked_lists/7-get_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * get_nodeint_at_index - returns the nth node of a linked list
  * @head: pointer
- * index: unsigned int
+ * @index: unsigned int
  * Return: 0 or NULL if it doesnt exist
  */
 
@@ -11,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int node_inc;
 
-	for(node_inc = 0; node_inc < index; node_inc++)
+	for (node_inc = 0; node_inc < index; node_inc++)
 	{
 		if (head == NULL)
 		{
