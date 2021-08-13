fac6caac84cd0605b816d2a3e841cbd1b6ceb307
calicowhoman <mikeyshrier@gmail.com>
2021-08-12 21:13:10 -0700

    git pushong value of bit to 1 and value of bit

--- 0x15-bit_manipulation/3-set_bit.c
+++ 0x15-bit_manipulation/3-set_bit.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
 
 /**
- * set_bit - F that sets the vale of a bit to 1 at agiven index
- * @n: 
- * @index: 
+ * set_bit - F that sets the vale of a bit to 1 at a given index
+ * @n: unsigned long int pointer
+ * @index: unsigned int
  * Return: 1 if worked or -1 if an error
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 
 {
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	*n ^= (1 << index);
 
-
-
-
+	return (1);
 }
