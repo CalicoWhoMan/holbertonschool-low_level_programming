afc6cc04a6161bc84a98855e2fc040663f5c5672
calicowhoman <mikeyshrier@gmail.com>
2021-08-12 20:55:18 -0700

    binary rep of a number

--- 0x15-bit_manipulation/1-print_binary.c
+++ 0x15-bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_binary - F that prints teh binary representation of a number
+ * print_binary - F that prints the binary representation of a number
  * @n:
  * Return: 0
  */
@@ -9,9 +9,9 @@
 void print_binary(unsigned long int n)
 
 {
-
-
-
-
-
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) + '0');
 }
