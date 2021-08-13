ae85769043b20f6761623d0d4e464ebf53486abd
calicowhoman <mikeyshrier@gmail.com>
2021-08-12 21:12:10 -0700

    2nd try pushing

--- 0x15-bit_manipulation/2-get_bit.c
+++ 0x15-bit_manipulation/2-get_bit.c
@@ -2,18 +2,19 @@
 
 /**
  * get_bit - F that returns teh value of a bit at a given index
- * @n: 
- * @index:
+ * @n: unsigned long int
+ * @index: unsigned long int
  * Return: The value of a bit at index or -1 if error
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 
 {
+	int bitsize;
 
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	bitsize = (n >> index) & 1;
 
-
-
-
-
+		return (bitsize);
 }
