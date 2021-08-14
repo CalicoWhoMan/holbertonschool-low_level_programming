c76d57aa64c277270c8ec04f2e92a80a0f8fe30c
calicowhoman <mikeyshrier@gmail.com>
2021-08-13 21:02:58 -0700

    fixed betty style coding

--- 0x15-bit_manipulation/0-binary_to_uint.c
+++ 0x15-bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,34 @@
 #include "holberton.h"
 #include <string.h>
 /**
- * int binary_to_uint - F that converts a binary number to an unsigned int
- * @b: 
+ * binary_to_uint - F that converts a binary number to an unsigned int
+ * @b: bytes
  * Return: converted numer or 0
  */
 
 unsigned int binary_to_uint(const char *b)
 
 {
-	unsigned int result;
-	int i, exponent;
+unsigned int result;
+int i, exponent;
 
-	if (!b)
-			return (0);
+if (!b)
+return (0);
 
-	result = 0;
+result = 0;
 
-	for (i = 0; b[i] != '\0'; i++)
+for (i = 0; b[i] != '\0'; i++)
 ;
-	for (i--, exponent = 1; i >= 0; i--, exponent *= 2)
-	{
-		if (b[i] != '0' && b[i] != '1')
-		{
-			return (0);
-		}
-		if (b[i] & 1)
-		{
-			result += exponent;
-		}
-	}
+for (i--, exponent = 1; i >= 0; i--, exponent *= 2)
+{
+if (b[i] != '0' && b[i] != '1')
+{
+return (0);
+}
+if (b[i] & 1)
+{
+result += exponent;
+}
+}
 return (result);
 }
