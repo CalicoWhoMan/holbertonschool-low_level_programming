21af3eff6024d527100845bcdb3413331325d1a5
calicowhoman <mikeyshrier@gmail.com>
2021-08-13 20:34:06 -0700

    files for 0 and 5

--- 0x15-bit_manipulation/0-binary_to_uint.c
+++ 0x15-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,5 @@
-#include "holerton.h"
-
+#include "holberton.h"
+#include <string.h>
 /**
  * int binary_to_uint - F that converts a binary number to an unsigned int
  * @b: 
@@ -9,7 +9,26 @@
 unsigned int binary_to_uint(const char *b)
 
 {
+	unsigned int result;
+	int i, exponent;
 
+	if (!b)
+			return (0);
 
+	result = 0;
 
+	for (i = 0; b[i] != '\0'; i++)
+;
+	for (i--, exponent = 1; i >= 0; i--, exponent *= 2)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		if (b[i] & 1)
+		{
+			result += exponent;
+		}
+	}
+return (result);
 }
--- 0x15-bit_manipulation/5-flip_bits.c
+++ 0x15-bit_manipulation/5-flip_bits.c
@@ -10,8 +10,10 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 {
+	n ^= m;
 
+	for (m = 0; n != 0; n >>= 1)
+		m += n & 1;
 
-
-
+	return (m);
 }
