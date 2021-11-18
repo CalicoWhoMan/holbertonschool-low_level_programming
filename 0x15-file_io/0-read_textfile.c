f61689a6d4e1baafd7c9e330ee3e4954d54fb011
CalicoWhoMan <mikeyshrier@gmail.com>
2021-11-18 03:14:28 +0000

    barebones of file 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *
+ * read_textfile - reads a file and prints to the POSIX stdrd output
+ * @filename: number of files to be read
+ * @letters: number of letters to be read into the file
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+
+
+} 
