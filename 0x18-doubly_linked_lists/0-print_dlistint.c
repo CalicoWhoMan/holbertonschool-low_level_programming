94c5375830117f1b476b731d8cbd111278a4307e
CalicoWhoMan <mikeyshrier@gmail.com>
2021-12-07 12:45:59 -0800

    0 added func that prints elements of a DLL

--- 0x18-doubly_linked_lists/0-print_dlistint.c
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -1,26 +1,22 @@
-#include "holberton.h"
+#include "lists.h"
 #include <string.h>
 
 /**
- * print_dlistint -
- * @h:
- * Return:
+ * print_dlistint - Function that prints all the elements of a dlistint_t list
+ * @h: pointer to a node
+ * Return: the number of nodes
  */
 
 size_t print_dlistint(const dlistint_t *h)
 
 {
+	int count = 0;
 
-
-
-
-
-
-
-
-
-
-
-
-
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
