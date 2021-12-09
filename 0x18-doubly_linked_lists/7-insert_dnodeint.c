6a2d4e93b97c6cdd0cedb1ead01b61220ec68f66
CalicoWhoMan <mikeyshrier@gmail.com>
2021-12-08 21:33:35 -0800

    bare + bettied

--- 0x18-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x18-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,12 +2,12 @@
 #include <string.h>
 
 /**
- * print_dlistint_backward -
- * @h:
- * Return:
+ * print_dlistint_backward - Function that prints all the elements of a
+ * DLL backwards
+ * @h: head of list
+ * Return: address of new node otherwise NULL
  */
 
 size_t print_dlistint_backward(const dlistint_t *h)
 {
-
 }
