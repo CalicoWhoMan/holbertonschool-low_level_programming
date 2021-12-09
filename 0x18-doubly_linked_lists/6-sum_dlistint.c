c2d25af06e8e2fbca0c680c8dfaba69992212a5d
CalicoWhoMan <mikeyshrier@gmail.com>
2021-12-08 20:44:24 -0800

    Function that returns the sum of all the data (n) of a DLL

--- 0x18-doubly_linked_lists/6-sum_dlistint.c
+++ 0x18-doubly_linked_lists/6-sum_dlistint.c
@@ -1,28 +1,18 @@
-#include "holberton.h"
+#include "lists.h"
 #include <string.h>
 
 /**
- * sum_dlistint -
- * @head:
- * Return:
+ * sum_dlistint - Function that returns the sum of all the data (n) of a DLL
+ * @head: head of list
+ * Return: Sum of all the data in (a) list otherwise 0
  */
 
+int sum_dlistint(dlistint_t *head)
+
 {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int inc;
+for (inc = 0; head != NULL; inc += head->n, head = head->next)
+{
+}
+return (inc);
 }
