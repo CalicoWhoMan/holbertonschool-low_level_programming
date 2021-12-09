090c6ae9adf42e6d4c1cc22981299cf4e9acd3d1
CalicoWhoMan <mikeyshrier@gmail.com>
2021-12-08 18:36:10 -0800

    added 02, main, exe; Function that adds a new node at beginning of a DLL

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -1,29 +1,25 @@
-#include "holberton.h"
+#include "lists.h"
 #include <string.h>
 
 /**
- * add_dnodeint -
- * @head:
- * @n:
- * Return:
+ * add_dnodeint - Function that adds a new node at the beginning of DLL
+ * @head: **pointer to head of node
+ * @n: value of n for node
+ * Return: new node
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-
 {
+	dlistint_t *nnode;
 
+	nnode = malloc(sizeof(dlistint_t *));
+	if (nnode)
+	{
+		nnode->n = n;
+		nnode->prev = NULL;
+		nnode->next = *head;
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	*head = nnode;
+	return (*head);
 }
