4380e0d38a35e3fc063296c1a1e5e61c1722ff7d
CalicoWhoMan <mikeyshrier@gmail.com>
2021-12-08 19:16:02 -0800

    04 Function that frees a DLL

--- 0x18-doubly_linked_lists/4-free_dlistint.c
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -1,18 +1,23 @@
-#include "holberton.h"
+#include "lists.h"
 #include <string.h>
 
 /**
- * free_dlistint -
- * @head:
- * Return:
+ * free_dlistint - Function that frees a DLL
+ * @head: head of node
+ * Return: freed up list
  */
 
 void free_dlistint(dlistint_t *head)
 
 {
+	dlistint_t *ru_node;
+	dlistint_t *fr;
 
-
-
-
-
+	fr = head;
+	while (fr != NULL)
+	{
+		ru_node = fr;
+		fr = fr->next;
+		free(ru_node);
+	}
 }
