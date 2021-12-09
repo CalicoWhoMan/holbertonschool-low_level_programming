bbd95061d4ed0e886a081d14e75325e30b244502
CalicoWhoMan <mikeyshrier@gmail.com>
2021-12-08 20:31:27 -0800

    Function that returns the nth node of a DLL

--- 0x18-doubly_linked_lists/5-get_dnodeint.c
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -1,22 +1,25 @@
-#include "holberton.h"
+#include "lists.h"
 #include <string.h>
 
 /**
- * get_dnodeint_at_index -
- * @head:
- * @index:
- * Return:
+ * get_dnodeint_at_index - Function that returns the nth node of a DLL
+ * @head: head of the list
+ * @index: index of list
+ * Return: pointer to node otherwise NULL
  */
 
-int sum_dlistint(dlistint_t *head)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 {
+	size_t inc = 0;
 
+	while (head != NULL)
+	{
+		if (inc == index)
+			break;
+		inc++;
+		head = head->next;
+	}
 
-
-
-
-
-
-
+	return (head);
 }
