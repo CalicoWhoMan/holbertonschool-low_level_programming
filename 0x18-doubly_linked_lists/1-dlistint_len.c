140807b582294adb6dd1b5da21983185903070e5
CalicoWhoMan <mikeyshrier@gmail.com>
2021-12-08 19:09:53 -0800

    01 Function that returns the number of elements in a linked DLL

--- 0x18-doubly_linked_lists/1-dlistint_len.c
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -1,18 +1,21 @@
-#include "holberton.h"
+#include "lists.h"
 #include <string.h>
 
 /**
- * dlistint_len -
- * @h:
- * Return:
+ * dlistint_len - Function that returns the # of elements in a linked DLL
+ * @h: pointer to a DoublyLinkedList AKA DLL
+ * Return: length of the DLL
  */
 
 size_t dlistint_len(const dlistint_t *h)
 
 {
+size_t length = 0;
 
-
-
-
-
+while (h != NULL)
+{
+h = h->next;
+length++;
+}
+return (length);
 }
