74833843a03649000ebd0c2b5c5166d3627b96f0
CalicoWhoMan <mikeyshrier@gmail.com>
2021-12-12 21:57:12 -0800

    empty files

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Function that prints a hash table
+ * @ht: hash table
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{}
---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Function that deletes a hash table
+ * *ht: hash table
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{}
