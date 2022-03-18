29a7ea7aa75b66fa06ea94b48a4ff4f01ad9aedb
CalicoWhoMan <mikeyshrier@gmail.com>
2022-03-17 20:36:10 -0700

    3rd try but revert

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
 	}
 	if (trim == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if ((close(file_start) == -1) || (close(file_end) == -1))
