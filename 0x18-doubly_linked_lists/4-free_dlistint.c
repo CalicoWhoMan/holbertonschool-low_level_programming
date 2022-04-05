8b490b5aed8c23a1885d87fbb057a3b0a85a463c
CalicoWhoMan <mikeyshrier@gmail.com>
2022-04-04 18:28:39 -0700

    StckO free list #4

---
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a linked list
+ * @head: head of list
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head; 
+		head = head->next;
+		free(tmp);
+	}
+}
