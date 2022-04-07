7fb5970e9027ba193dbb42a0f934d5756e961cb1
CalicoWhoMan <mikeyshrier@gmail.com>
2022-04-06 18:53:23 -0700

    .2

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((unsigned char *)key, ht->size);
 	new = ht->array[index];
-	while(new)
+	while (new)
 	{
 		if (strcmp(new->key, key) == 0)
 		{
